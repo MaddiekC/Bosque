e87ac9d53ac424c6509c2a932ebe0cab3766843f
MaddiekC <m.coooolllm@gmail.com>
2025-10-22 11:35:47 -0500

    estilo

--- FRONTEND/src/environment/environment.prod.ts
+++ FRONTEND/src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.0.4:8080/api'  // URL para producción
+  apiUrl: 'http://127.0.0.1:8080/api'  // URL para producción
 };
--- FRONTEND/src/environment/environment.ts
+++ FRONTEND/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.0.4:8080/api'  // Cambia según tu URL Laravel
+  apiUrl: 'http://127.0.0.1:8080/api'  // Cambia según tu URL Laravel
   //apiUrl: 'http://192.168.0.4:8080/api'
   //http://127.0.0.1:8080/api
 };
